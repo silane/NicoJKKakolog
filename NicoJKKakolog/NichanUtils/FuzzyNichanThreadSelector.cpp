227988e19f73a2ec3af83199ff5c3e1a9f496729
silane <michiwatsuki@gmail.com>
2016-07-30 22:23:35 +0900

    2chの類似スレ選択が間違っていたのを修正

--- NicoJKKakolog/NichanUtils/FuzzyNichanThreadSelector.cpp
+++ NicoJKKakolog/NichanUtils/FuzzyNichanThreadSelector.cpp
@@ -3,6 +3,7 @@
 #include "../../NichanParser/NichanParser.h"
 #include <limits>
 #include <algorithm>
+#include <utility>
 
 #undef min//win32APIのマクロ回避
 #undef max
@@ -12,18 +13,18 @@ namespace NicoJKKakolog
 	int FuzzyNichanThreadSelector::LevenshteinDistance(const std::string &str1, const std::string &str2)
 	{
 		std::vector<std::vector<int>> d(str1.size()+1);
-		for (int i = 0; i <= str1.size(); i++) 
+		for (decltype(d)::size_type i = 0; i <= str1.size(); i++) 
 		{
 			d[i].resize(str2.size() + 1);
-			d[i][0] = i;
+			d[i][0] = (int)i;
 		}
-		for (int i = 0; i <= str2.size(); i++)
+		for (decltype(d)::size_type i = 0; i <= str2.size(); i++)
 		{
-			d[0][i] = i;
+			d[0][i] = (int)i;
 		}
 
-		for (int i1 = 1; i1 <= str1.size(); i1++)
-			for (int i2 = 1; i2 <= str2.size(); i2++)
+		for (decltype(d)::size_type i1 = 1; i1 <= str1.size(); i1++)
+			for (decltype(d)::size_type i2 = 1; i2 <= str2.size(); i2++)
 			{
 				const int cost = (str1[i1] == str2[i2]) ? 0 : 1;
 				d[i1][i2] = std::min({ d[i1 - 1][i2] + 1,d[i1][i2 - 1] + 1,d[i1 - 1][i2 - 1] + cost });
@@ -40,23 +41,36 @@ namespace NicoJKKakolog
 	std::vector<std::string> FuzzyNichanThreadSelector::Get(const ChannelInfo &channel, const std::chrono::system_clock::time_point &t)
 	{
 		auto board = Nichan::ParseBoardFromUrl(this->boardUrl);
-		std::vector<int> distances(board.thread.size());
 
-		std::transform(std::begin(board.thread), std::end(board.thread), std::begin(distances),
-			[this](const Nichan::Thread &th) {return LevenshteinDistance(th.title, this->threadTitle); });
-
-		int min_distance=0;
-		auto itr=std::min(std::begin(distances), std::end(distances));
-		if (itr != std::end(distances))
-			min_distance = *itr;
-
-		std::vector<std::string> ret;
-		for (int i=0;i<distances.size();i++)
+		//1000まで埋まっているスレを除外
+		for (auto itr = std::begin(board.thread); itr != std::end(board.thread); ++itr)
 		{
-			if (distances[i] - min_distance<=6)
-				ret.push_back(board.thread[i].url);
+			if (itr->title.compare(itr->title.size() - 6, 6, u8"(1001)") == 0)
+				itr=board.thread.erase(itr);
 		}
 
+		typedef std::pair<Nichan::Thread *, int> item_type;
+		std::vector<item_type> distances(board.thread.size());
+
+		std::transform(std::begin(board.thread), std::end(board.thread), std::begin(distances),
+			[this](Nichan::Thread &th) {return item_type(&th, LevenshteinDistance(th.title, this->threadTitle)); });
+
+		std::sort(std::begin(distances), std::end(distances),
+			[](const item_type &item1, const item_type &item2) {return item1.second < item2.second; });
+		
+		
+		if (distances.size() == 0)
+			return{};
+
+		std::vector<std::string> ret;
+		int min_distance = distances[0].second;
+		for (const auto &item : distances)
+		{
+			if (item.second - min_distance >= 30)
+				break;
+			ret.push_back(item.first->url);
+		}
+		
 		return ret;
 	}
 }
